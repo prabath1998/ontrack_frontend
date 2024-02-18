1294481693617bf64d2211c23463db494446e9f3
prabath1998 <prabhathudayanga1998@gmail.com>
2024-02-18 22:18:51 +0530

    UI improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./themes/darktheme";
 import Navbar from "./Pages/Navbar/Navbar";
+import Sidebar from "./Pages/Sidebar/Sidebar";
 
 function App() {
   return (
    <ThemeProvider theme={darkTheme}>
     <Navbar/>
+    <Sidebar/>
    </ThemeProvider>
   );
 }
--- src/Pages/Sidebar/Sidebar.jsx
+++ src/Pages/Sidebar/Sidebar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Avatar } from '@mui/material';
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='min-h-[85vh] flex flex-col justify-center fixed w-[20vw]'>
+      <div className='space-y-5 h-full'>
+        <div className='flex justify-center'>
+            <Avatar src='https://mui.com/static/images/avatar/1.jpg'/>
+        </div>
+      </div>
     </div>
   )
 }
